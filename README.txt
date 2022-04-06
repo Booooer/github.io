ea48e70e9b034c8b00678332ffe39a9f31e93e99
Booooer <95856528+Booooer@users.noreply.github.com>
2022-04-07 02:10:39 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Ссылка на макеты:
+https://www.figma.com/file/BhUSY4mWkPe3nZh66xRb3B/AnimeShop?node-id=0%3A1
---
+++ scripts/burger.js
@@ -0,0 +1,4 @@
+function onclickMenu(){
+  document.getElementById("burger_btn").classList.toggle("active");
+  document.getElementById("menu").classList.toggle("active");
+}
